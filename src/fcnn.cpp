c477e0d8528d89df1931383b7f849c3beff001cb
Daniel Suess <daniel@dsuess.me>
2017-04-14 14:59:32 +1000

    Implement weight getter/setter

--- src/fcnn.cpp
+++ src/fcnn.cpp
@@ -13,11 +13,14 @@ using namespace std;
 
 
 typedef vector<size_t> shape_t;
-
+typedef Eigen::Matrix<double,
+                      Eigen::Dynamic,
+                      Eigen::Dynamic,
+                      Eigen::RowMajor> matrix_t;
 
 typedef struct NNLayer {
     ActivationFunction activation;
-    Eigen::MatrixXd w;
+    matrix_t w;
 } NNLayer;
 
 
@@ -28,10 +31,9 @@ public:
     : layers(neurons.size())
     {
         const auto n_layers = neurons.size();
-        // TODO Proper error handling
-        // TODO Get rid of 1-output neuron constraint
-        assert(n_layers >= 2);
-        assert(*neurons.end() == 1);
+        if(n_layers < 1) {
+            throw invalid_argument("Number of layers is too small");
+        }
 
         // Create temporary shapes array to unify loop below
         size_t shapes[neurons.size() + 1];
@@ -41,7 +43,7 @@ public:
         // Initialize the weights with zeros
         for (size_t n = 0; n < n_layers; ++n) {
             // +1 to accomodate biases
-            layers[n].w = Eigen::MatrixXd::Zero(shapes[n + 1], shapes[n] + 1);
+            layers[n].w = matrix_t::Zero(shapes[n + 1], shapes[n] + 1);
             layers[n].activation = sigmoid;
         }
     }
@@ -61,6 +63,26 @@ public:
         return result;
     }
 
+
+    matrix_t &get_weights(const size_t layer)
+    {
+        return layers[layer].w;
+    }
+
+
+    void set_weights(const size_t layer, Eigen::Ref<matrix_t> weight)
+    {
+        if((weight.rows() != layers[layer].w.rows()) ||
+           (weight.cols() != layers[layer].w.cols())) {
+            throw invalid_argument("Set weight has wrong shape");
+        }
+        layers[layer].w = weight;
+    }
+
+
+
+
+
 private:
     vector<NNLayer> layers;
 };
@@ -75,7 +97,9 @@ PYBIND11_PLUGIN(fcnn)
         .def_property_readonly("n_inputs", &FCNN::n_inputs)
         .def_property_readonly("n_outputs", &FCNN::n_outputs)
         .def_property_readonly("hlayers", &FCNN::hlayers)
-        .def_property_readonly("neurons", &FCNN::neurons);
+        .def_property_readonly("neurons", &FCNN::neurons)
+        .def("get_weights", &FCNN::get_weights, "layer"_a)
+        .def("set_weights", &FCNN::set_weights, "layer"_a, "weight"_a);
 
     return m.ptr();
 }
--- tests/fcnn_test.py
+++ tests/fcnn_test.py
@@ -1,11 +1,60 @@
-import fcnn
+import numpy as np
 import pytest as pt
+from fcnn import FCNN
 
 
-@pt.mark.parametrize('neurons', [(1, ), (10, 5, 3, 10)])
-def test_init(neurons):
-    a = fcnn.FCNN(5, neurons)
-    assert a.hlayers == len(neurons) - 1
-    assert a.n_inputs == 5
-    assert a.n_outputs == neurons[-1]
-    assert tuple(a.neurons) == neurons
+@pt.fixture(scope="module")
+def rgen():
+    return np.random.RandomState(seed=3476583865)
+
+
+@pt.mark.parametrize('shape', [(5, 1), (10, 5, 3, 10)])
+def test_init(shape):
+    nn = FCNN(shape[0], shape[1:])
+    assert nn.hlayers == len(shape) - 2
+    assert nn.n_inputs == shape[0]
+    assert nn.n_outputs == shape[-1]
+    assert tuple(nn.neurons) == shape[1:]
+
+
+def test_get_set_weights():
+    shape = (5, 8, 1)
+    nn = FCNN(shape[0], shape[1:])
+
+    for i in range(len(shape) - 1):
+        # +1 Due to implicit weights
+        assert nn.get_weights(i).shape == (shape[i + 1], shape[i] + 1)
+
+    new_weight = np.random.randn(shape[1], shape[0] + 1)
+    nn.set_weights(0, new_weight)
+    np.testing.assert_array_equal(new_weight, nn.get_weights(0))
+
+
+def test_get_set_weights_permissions():
+    shape = (5, 1)
+    nn = FCNN(shape[0], shape[1:])
+    new_weight = np.random.randn(shape[1], shape[0] + 1)
+    new_weight_copy = new_weight.copy()
+    nn.set_weights(0, new_weight)
+
+    new_weight[:] = 0
+    nn_weight = nn.get_weights(0)
+    np.testing.assert_array_equal(new_weight_copy, nn_weight)
+    assert nn_weight.flags['OWNDATA']
+    assert nn_weight.flags['WRITEABLE']
+
+    del nn
+    nn_weight[:] = 0
+
+
+def test_set_weights_exception():
+    shape = (5, 1)
+    nn = FCNN(shape[0], shape[1:])
+    new_weight = (np.random.randn(shape[1], shape[0]))
+
+    try:
+        nn.set_weights(0, new_weight)
+    except ValueError:
+        pass
+    else:
+        raise AssertionError("Setting weight with wrong shape should raise error")
